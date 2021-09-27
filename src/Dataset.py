9f725cd884e55b179496e98144399de56c9837a8
npangaro <90272870+npangaro@users.noreply.github.com>
2021-09-26 20:33:19 -0400

    Changed path
    Made it so path should work for everyone.

--- src/Dataset.py
+++ src/Dataset.py
@@ -2,12 +2,12 @@ import requests
 import os
 
 directory = 'DATASET'
-parent_dir = 'Macintosh HD/Users/nickpangaro/2021-Facial-Age-Gender-Recognition/'
+parent_dir = '2021-Facial-Age-Gender-Recognition/'
 
 path = os.path.join(parent_dir, directory)
 
 os.mkdir(path)
 
 r = requests.get('https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_0.tar')  
-with open('Macintosh HD/Users/nickpangaro/2021-Facial-Age-Gender-Recognition/DATASET', 'wb') as f:
+with open('2021-Facial-Age-Gender-Recognition/DATASET', 'wb') as f:
     f.write(r.content)
