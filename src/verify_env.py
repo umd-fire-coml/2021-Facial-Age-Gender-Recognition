8de8fdf454433cf301724d3c13ccac55e21df2db
smitpatel1337 <spatel50@terpmail.umd.edu>
2021-09-28 19:13:06 -0400

    Test for Requirements.txt
    Checks if requirements.txt's packages are installed

---
+++ src/verify_env.py
@@ -0,0 +1,30 @@
+import os
+
+def verify_env():
+
+    requirements = open("requirements.txt", "r").read().splitlines()
+    env_pkg = []
+    is_pkgs_installed = True
+    isPkgInstalled = False
+    
+    for aline in requirements:
+        requiremnts = aline.strip()
+        env_pkg.append(requiremnts)
+
+    installed = []
+    installed = os.popen('pip list').read().split()
+
+    for package in env_pkg:
+        if package in installed:
+            isPkgInstalled = True
+        else:
+            is_pkgs_installed = False
+            isPkgInstalled = False
+        if isPkgInstalled is False:
+            print(f"{package} is not installed!")
+    
+    if is_pkgs_installed is True:
+        print("All installed!")
+        return True
+    else:
+        return False
