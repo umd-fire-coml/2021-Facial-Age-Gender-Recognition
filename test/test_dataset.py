0dfa62f4ff655918c46e3137a89206d77fe83878
npangaro <90272870+npangaro@users.noreply.github.com>
2021-09-28 17:55:13 -0400

    Test fix #3

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -4,7 +4,7 @@ from os import path
 from src.Dataset import Downloader
 
 
-def folder_present():
+def test_folder_present():
     Downloader.downloader("https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar")
     path = os.getcwd()
     parent = os.path.dirname(path)
