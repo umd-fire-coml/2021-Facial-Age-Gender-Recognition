83394f311851919713e7dd8d2099b0ba6782bccb
npangaro <90272870+npangaro@users.noreply.github.com>
2021-09-28 17:53:16 -0400

    Test fix #2

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -5,10 +5,9 @@ from src.Dataset import Downloader
 
 
 def folder_present():
+    Downloader.downloader("https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar")
     path = os.getcwd()
     parent = os.path.dirname(path)
     os.path.join(parent)
     os.path.join('src')
-    Downloader.downloader("https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar")
-
     assert not (os.listdir('wiki_crop'))
