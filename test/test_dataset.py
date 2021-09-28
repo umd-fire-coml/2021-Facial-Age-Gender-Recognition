99c8c8cfaaf8f0a3f9089a48640912e006f66c84
npangaro <90272870+npangaro@users.noreply.github.com>
2021-09-28 17:48:15 -0400

    Test fix

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -3,19 +3,12 @@ import os
 from os import path
 from src.Dataset import Downloader
 
-path = os.getcwd()
-parent = os.path.dirname(path)
-
-os.path.join(parent)
-os.path.join('src')
-
-
-Downloader.downloader("https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar")
 
 def folder_present():
-    if (os.path.exists('dataset.tar')):
-        print("Fail")
-        return (False)
-    else:
-        print("Success")
-        return (True)
+    path = os.getcwd()
+    parent = os.path.dirname(path)
+    os.path.join(parent)
+    os.path.join('src')
+    Downloader.downloader("https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar")
+
+    assert not (os.listdir('wiki_crop'))
