7dd6db0010075f0a627b8f7d22e9eaeb6e3724d3
omhpathak-tech <ompathak@umd.edu>
2021-09-28 16:54:57 -0400

    File Checker

--- src/filescript.py
+++ src/filescript.py
@@ -1,3 +1,18 @@
-import sys, glob
-from os import path
+import os
 
+DATA_FOLDER = 'PATH'
+
+totalFiles = 0
+totalDir = 0
+
+for base, dirs, files in os.walk(DATA_FOLDER):
+    print('Searching in : ',base)
+    for directories in dirs:
+        totalDir += 1
+    for Files in files:
+        totalFiles += 1
+
+
+print('Total number of files',totalFiles)
+print('Total Number of directories',totalDir)
+print('Total:',(totalDir + totalFiles))
