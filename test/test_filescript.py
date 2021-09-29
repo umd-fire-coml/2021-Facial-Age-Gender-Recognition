84aa4926e153d5334647a684cdfdeb2d69672921
omhpathak-tech <ompathak@umd.edu>
2021-09-29 12:07:25 -0400

    adding files --

--- test/test_filescript.py
+++ test/test_filescript.py
@@ -3,10 +3,10 @@ from filescript import check_files, check_jpg
 
 # they should return false on remote because the file path is not specified 
 def test_jpg():
-    assert bool(check_jpg()) != False
+    assert bool(check_jpg()) == False
 
 def test_numfiles(): 
-    assert bool(check_files()) != False
+    assert bool(check_files()) == False
 
 if __name__ == "__main__":
     test_jpg()
